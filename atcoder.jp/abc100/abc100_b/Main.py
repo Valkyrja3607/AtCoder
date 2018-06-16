2134c47be1e24b383b79541762e1c3a8a59f23bc
Valkyrja <purinjolly@gmail.com>
2018-06-16 22:00:19 +0900

    [AC] abc100 abc100_b

---
+++ atcoder.jp/abc100/abc100_b/Main.py
@@ -0,0 +1,16 @@
+d,n=map(int,input().split())
+
+if n!= 100:
+    if d==0:
+        print(n)
+    elif d==1:
+        print(n*100)
+    elif d==2:
+        print(n*10000)
+else:
+    if d==0:
+        print(101)
+    elif d==1:
+        print(10100)
+    elif d==2:
+        print(1010000)
