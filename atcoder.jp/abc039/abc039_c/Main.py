a806405264ef1e1038bc245ee170f37a9c3f06c4
Valkyrja <purinjolly@gmail.com>
2018-12-19 22:29:49 +0900

    [AC] abc039 abc039_c

---
+++ atcoder.jp/abc039/abc039_c/Main.py
@@ -0,0 +1,16 @@
+s=input()
+#WBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBW
+if s=="WBWBWWBWBWBWWBWBWWBW":
+    print("Do")
+if s=="WBWWBWBWBWWBWBWWBWBW":
+    print("Re")
+if s=="WWBWBWBWWBWBWWBWBWBW":
+    print("Mi")
+if s=="WBWBWBWWBWBWWBWBWBWW":
+    print("Fa")
+if s=="WBWBWWBWBWWBWBWBWWBW":
+    print("So")
+if s=="WBWWBWBWWBWBWBWWBWBW":
+    print("La")
+if s=="WWBWBWWBWBWBWWBWBWWB":
+    print("Si")
