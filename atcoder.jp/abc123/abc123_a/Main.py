f4dcce83b9a1f3055bda7c40e5b49180924e5cdf
Valkyrja <purinjolly@gmail.com>
2019-04-06 21:04:14 +0900

    [AC] abc123 abc123_a

---
+++ atcoder.jp/abc123/abc123_a/Main.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+k=int(input())
+
+if e-a<=k and e-b<=k and e-c<=k and e-d<=k:
+    print("Yay!")
+else:
+    print(":(")
+
