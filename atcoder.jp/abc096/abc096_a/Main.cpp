b03689165d37b3c61c858f8814223ff9d4dfa368
Valkyrja <purinjolly@gmail.com>
2019-08-25 22:33:56 +0900

    [AC] abc096 abc096_a

---
+++ atcoder.jp/abc096/abc096_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // Your code here!
+    int a,b;
+    cin>>a>>b;
+    if (a<=b){
+        cout<<a<<endl;
+    }else{
+        cout<<a-1<<endl;
+    }
+}
