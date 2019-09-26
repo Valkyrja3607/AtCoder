5c5db46d947e31178cfbc029db8f8fef21c583af
Valkyrja <purinjolly@gmail.com>
2019-09-26 18:42:30 +0900

    [AC] APG4b APG4b_cs

---
+++ atcoder.jp/APG4b/APG4b_cs/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<365*24*60*60<<endl;
+    cout<<2*365*24*60*60<<endl;
+    cout<<5*365*24*60*60<<endl;
+    cout<<10*365*24*60*60<<endl;
+}
