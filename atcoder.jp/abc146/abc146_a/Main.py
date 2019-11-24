b8f1fea2d2882064c5c2f3f684710a8c01020b81
‘Valkyrja3607; GIT_AUTHOR_EMAIL=‘purinjolly@gmail.com <purinjolly@gmail.com>
2019-11-24 21:02:37 +0900

    [AC] abc146 abc146_a

---
+++ atcoder.jp/abc146/abc146_a/Main.py
@@ -0,0 +1,15 @@
+s=input()[:2]
+if s=="SU":
+    print(7)
+elif s=="MO":
+    print(6)
+elif s=="TU":
+    print(5)
+elif s=="WE":
+    print(4)
+elif s=="TH":
+    print(3)
+elif s=="FR":
+    print(2)
+else:
+    print(1)
