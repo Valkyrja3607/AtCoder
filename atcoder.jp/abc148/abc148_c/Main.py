84190fc917137283394abad9ae05ac2de6e98df2
‘Valkyrja3607; GIT_AUTHOR_EMAIL=‘purinjolly@gmail.com <purinjolly@gmail.com>
2019-12-22 21:03:35 +0900

    [AC] abc148 abc148_c

---
+++ atcoder.jp/abc148/abc148_c/Main.py
@@ -0,0 +1,11 @@
+a,b=[int(j) for j in input().split()]
+def gcd(x,y):
+    if y==0:
+        return x
+    else:
+        return gcd(y,x%y)
+
+
+def lcm(x,y):
+    return x*y//gcd(x,y)
+print(lcm(a,b))
