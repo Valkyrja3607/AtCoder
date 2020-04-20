2a0cdcde38d3079d2f8c6a9f6e3203a9949c6d02
‘Valkyrja3607; GIT_AUTHOR_EMAIL=‘purinjolly@gmail.com <purinjolly@gmail.com>
2020-04-20 17:12:51 +0900

    [AC] diverta2019 diverta2019_d

---
+++ atcoder.jp/diverta2019/diverta2019_d/Main.py
@@ -0,0 +1,16 @@
+n=int(input())
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+    divisors.sort()
+    return divisors
+l=make_divisors(n)
+ans=0
+for i in l:
+    if i-1>(n**0.5):
+        ans+=i-1
+print(ans)
