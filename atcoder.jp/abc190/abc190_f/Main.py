08c2167638696f64f550249e6eb5df35beebf289
Valkyrja <purinjolly@gmail.com>
2021-02-01 02:17:21 +0900

    [AC] abc190 abc190_f

---
+++ atcoder.jp/abc190/abc190_f/Main.py
@@ -0,0 +1,35 @@
+n=int(input())
+a=list(map(int,input().split()))
+l=a.copy()
+def mergecount(A):
+    cnt = 0
+    n = len(A)
+    if n>1:
+        A1 = A[:n>>1]
+        A2 = A[n>>1:]
+        cnt += mergecount(A1)
+        cnt += mergecount(A2)
+        i1=0
+        i2=0
+        for i in range(n):
+            if i2 == len(A2):
+                A[i] = A1[i1]
+                i1 += 1
+            elif i1 == len(A1):
+                A[i] = A2[i2]
+                i2 += 1
+            elif A1[i1] <= A2[i2]:
+                A[i] = A1[i1]
+                i1 += 1
+            else:
+                A[i] = A2[i2]
+                i2 += 1
+                cnt += n//2 - i1
+    return cnt
+ans=mergecount(a)
+print(ans)
+for i in range(n-1):
+    b=l[i]
+    ans-=b
+    ans+=n-b-1
+    print(ans)
